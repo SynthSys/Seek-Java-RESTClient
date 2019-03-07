24f24d716ead97485705393f0da170b1cf4dc0b4
Johnny Hay <j.hay@epcc.ed.ac.uk>
2019-03-07 13:06:36 +0000

    Test package and JUnit

---
+++ src/test/java/ed/synthsys/seek/api/ReadApiClientTest.java
@@ -0,0 +1,38 @@
+package ed.synthsys.seek.api;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author jhay
+ */
+public class ReadApiClientTest {
+    
+    public ReadApiClientTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
